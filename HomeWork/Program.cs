d3bcc323cb4f36e7c567bd8a8a52085826a2e5e0
github_username <you@domain.com>
2022-03-15 12:31:33 +0400

    added task

---
+++ HomeWork/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Daragin max gulle tutumunu yazin(ancaq reqem) :");
+            int bulletcapacityofcomb = Int32.Parse(Console.ReadLine());
+            Console.Write("Daragda olan gulle sayini daxil edin(ancaq reqem) :");
+            int bulletcountincomb = Int32.Parse(Console.ReadLine());
+            Console.Write("Daragin doshalma vaxtini qeyd edin(ancaq reqem) :");
+            double timeofdischargeofcomb = double.Parse(Console.ReadLine());
+            Console.Write("Silahin modunu secin true - auto, false - single(ancaq true,false))");
+            bool autoorsingle = bool.Parse(Console.ReadLine());
+            Console.Write("-------------------------------------------");
+
+            Weapon weapon = new Weapon(bulletcapacityofcomb,bulletcountincomb,timeofdischargeofcomb, autoorsingle);
+             
+
+            string choice = "0";
+            while (choice != "6")
+            {
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("1 - Shoot methodu  2 - Fire methodu  3 -GetRemainBulletCount methodu  4 - Reload  5 - ChangeFireMode  6 - Proqrami dayandirmaq ucun");
+
+                
+                 Console.Write("Reqem daxil edin : ");
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        weapon.GetRemainBulletCount();
+                        int bullet = weapon.GetRemainBulletCount();
+                        if (bullet == -1) Console.WriteLine("Maxsdan artiq gulle qoyumaq olmaz");
+                        else Console.WriteLine("Daragin tam dolmasi ucun lazim olan gulle sayi :" + bullet);   
+                        
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.WriteLine("Program darayndirildi......");
+                        break;
+                    case "0":
+                        weapon.Info();
+                        break;
+                       
+                    default:
+                        Console.WriteLine("------------------------------------------");
+                        Console.WriteLine("1,2,3,4,5,6,0 Reqemlerinden istifade edin");
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
---
+++ HomeWork/Weapon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    class Weapon
+    {
+        public int bulletCapacityOfComb;
+        public int bulletCountInComb;
+        public double timeOfDischargeOfComb;
+        public bool autoOrSingle;
+
+
+        public Weapon(int bulletCapacityOfComb, int bulletCountInComb, double timeOfDischargeOfComb, bool autoOrSingle)
+        {
+            this.bulletCapacityOfComb = bulletCapacityOfComb; this.bulletCountInComb = bulletCountInComb; this.timeOfDischargeOfComb = timeOfDischargeOfComb; this.autoOrSingle = autoOrSingle;
+           
+        }
+        public void Info()
+        {
+            Console.WriteLine("Daragin max tutumu :"+bulletCapacityOfComb);
+            Console.WriteLine("Daragin icinde olan gulle sayi :"+bulletCountInComb);
+            Console.WriteLine("Daragin boshalma vaxti :"+timeOfDischargeOfComb+" san");
+            Console.WriteLine("Silahin atish rejmi (auto) :"+autoOrSingle);
+        }
+        public void Shoot()
+        {
+            if (bulletCountInComb == 0) Console.WriteLine("silahda gulle yoxdur :(");
+            else bulletCountInComb -= 1; Console.WriteLine("silahdan bir gulle atildi---->");
+                        
+        }
+        public void Fire()
+        {
+            if (bulletCountInComb == 0) Console.WriteLine("Silahda gulle yoxdur:(");                     
+            else
+            {
+                double firetime = bulletCountInComb * timeOfDischargeOfComb / bulletCapacityOfComb ;
+                Console.WriteLine(firetime+ "san");
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            if (bulletCapacityOfComb>bulletCountInComb) return bulletCapacityOfComb - bulletCountInComb;           
+            else return -1;
+        }
+        public void Reload()
+        {
+            if (bulletCapacityOfComb>bulletCountInComb)
+            {
+                int needbullet = bulletCapacityOfComb - bulletCountInComb;
+                bulletCountInComb = bulletCountInComb + needbullet;
+                Console.WriteLine("Daraga elave olunan gulle sayi  :" + needbullet + "eded");
+            }
+            else if (bulletCapacityOfComb == bulletCountInComb)
+            {
+                Console.WriteLine("Darag doludur");
+            }
+
+        }
+        public void ChangeFireMode()
+        {
+            Console.WriteLine("(True auto mod)(False single mod)");
+            bool secim = bool.Parse(Console.ReadLine());
+            if (secim == true)
+            {
+                autoOrSingle = true;
+            }
+            else if (secim == false)
+            {
+                autoOrSingle = false;
+            }
+            else 
+            {
+                Console.WriteLine("True ve ya False'den istifade edin");
+            }              
+        }
+
+    }
+}
